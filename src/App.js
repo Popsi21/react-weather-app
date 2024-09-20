6c52636dddacff5f7fbaaee4f2a30addbc50ad9a
Popsi21 <paulinetravers7@gmail.com>
2024-09-20 14:53:34 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is open-sourced on{" "}
-        <a href="https://github.com/Popsi21/react-weather-app" target="_blank">
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is open-sourced on{" "}
+          <a
+            href="https://github.com/Popsi21/react-weather-app"
+            target="_blank"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
