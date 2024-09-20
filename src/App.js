f3e282c6a4b5c4e9b87b858ec0bd700929a507d4
Popsi21 <paulinetravers7@gmail.com>
2024-09-20 14:43:30 +0200

    added github referral

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is open-sourced on{" "}
+        <a href="https://github.com/Popsi21/react-weather-app" target="_blank">
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
