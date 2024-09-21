6b2a8a4f5a2f393a91456e6360b5de3502976d69
Popsi21 <paulinetravers7@gmail.com>
2024-09-21 13:10:07 +0200

    added api intergration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import React from "react";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather defaultCity="New York" />
         <footer>
           This project is open-sourced on{" "}
           <a
